48fa211401b69d24a14aed5998553d9483b05eed
Bharat235-beep <rajkumar45burail@gmail.com>
2024-09-05 13:56:56 +0530

    fixing base url

--- src/helper/httpAxios.js
+++ src/helper/httpAxios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const httpAxios=axios.create({
-    baseURL:'https://task-manager-gamma-liart.vercel.app/' || 'http://localhost:3000/',
+    baseURL:'https://task-manager-gamma-liart.vercel.app/',
 })
